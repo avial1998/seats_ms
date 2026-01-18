05e4709c630ba75bec3751091a1223a10dec8214
avinash <avial1998@gmail.com>
2026-01-18 19:16:42 +0530

    Initial commit.

---
+++ src/test/java/com/avinash/learning/ticketing/system/seats_ms/SeatsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avinash.learning.ticketing.system.seats_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
